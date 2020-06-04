0771f63889af2722a783a1e7c94b2601e81b14c1
Jongirsan <jake-sunshine@hotmail.com>
2020-06-04 19:25:44 +0200

    add base file structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Javascript-clock
 Javascript Clock.
+written by Gabor, Garajszki (Codeberry lession)
+2020.06.04.
---
+++ ora.js
@@ -0,0 +1,19 @@
+function colorClock()
+{
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (hours<10) {hours = '0' + hours;}
+    if (minutes<10) {minutes = '0' + minutes;}
+    if (seconds<10) {seconds = '0' + seconds;}
+    var clockFace = hours+'-'+minutes+'-'+seconds;
+    var hexColor = "#" + hours + minutes + seconds;
+    document.body.style.background = hexColor;
+    document.getElementById('clock').innerHTML = clockFace;
+    setTimeout(function() { colorClock(); }, 1000);
+    /* =================================================*/
+    /* Set Time Out : setTimeout(function(){  }, 5000); */
+    /* =================================================*/
+}
+colorClock();
